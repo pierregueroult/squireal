2c7054836a900e777431939144c786974b96d014
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-02-13 14:46:25 +0100

    Prémisse de PWA

---
+++ app/Views/pages/download.php
@@ -0,0 +1,5 @@
+<main>
+  <button>
+    download
+  </button>
+</main>
--- app/Views/templates/start.php
+++ app/Views/templates/start.php
@@ -5,16 +5,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php
-
   $styleUrl = base_url() . "css/style.css";
   echo "<link rel='stylesheet' href='$styleUrl'>";
   // echo "<script src='https://cdn.tailwindcss.com'></script>";
-  
   ?>
   <title>
     SquiReal 🐿️ |
     <?= esc($title) ?>
   </title>
+  <link rel="manifest" href="<?= base_url() ?>manifest.webmanifest">
 </head>
 
 <body>
