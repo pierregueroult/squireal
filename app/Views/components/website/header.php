0b30e7946877525e2cf8a32e708cb80a1a6297fa
Pierre Guéroult <contact@pierregueroult.dev>
2024-02-14 18:30:58 +0100

    Fix Icône du Header

--- app/Views/components/website/header.php
+++ app/Views/components/website/header.php
@@ -1,7 +1,7 @@
 <header
   class="h-16 lg:h-24 fixed z-30 top-4 bg-foreground w-near-full max-w-screen-2xl left-1/2 -translate-x-1/2 flex border-2 rounded-xl border-text shadow-pop items-center justify-between px-8 sm:flex-row-reverse md:flex-row ">
   <button
-    class="lg:hidden flex items-center justify-center h-12 w-12 rounded-md bg-foreground text-foreground text-text border-text"
+    class="md:hidden flex items-center justify-center h-12 w-12 rounded-md bg-foreground text-foreground text-text border-text"
   >
   <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
   </button>
