a1fa725ae2252d695fdb011ba436b747202181c2
lefebsa9 <sachalefebvre15@gmail.com>
2024-02-20 13:43:21 +0100

    paragraphe page mission

--- app/Views/pages/mission.php
+++ app/Views/pages/mission.php
@@ -1,4 +1,13 @@
 <main>
 <h1>Our Missions</h1>
 <h3>SquiReal's mission is environmental, the goal is to raise awareness and instill ecological values ​​in order to be able to face current climate issues for large numbers of users.</h3>
+<p>SquiReal is passionately committed to a crucial environmental mission, focused on raising awareness and instilling essential ecological values. In a world facing unprecedented climate challenges, our goal is to mobilize a broad audience to address these crucial issues.
+
+Our vision is based on the conviction that each individual, whatever their situation or origin, has a role to play in preserving our planet. Through our initiatives and platforms, we aspire to educate and inspire people to adopt more sustainable practices in their daily lives.
+
+SquiReal offers a multitude of resources and tools to support users on their journey to a more environmentally friendly lifestyle. Whether through practical advice, educational resources or engaged communities, we aim to provide comprehensive support to anyone looking to make a positive difference to the planet.
+
+We firmly believe in the importance of collective action. By joining forces, we can have a significant impact on the fight against climate change and the preservation of biodiversity. Every little bit counts, and together we have the power to create a greener, more sustainable future for generations to come.
+
+Through our environmental mission, SquiReal strives to catalyze a mindset shift and inspire a new generation of environmental advocates. We are determined to meet the challenges before us and work together towards a future where harmony between man and nature prevails.</p>
 </main>
