6bce44d8c8593dd2f9233f49b8fa0e9f50d4e897
lefebsa9 <sachalefebvre15@gmail.com>
2024-02-20 14:55:27 +0100

    padding paragraphe et mise image en gros à droite

--- app/Views/pages/mission.php
+++ app/Views/pages/mission.php
@@ -1,12 +1,14 @@
-<main class="bg-background text-text flex flex-wrap justify-between">
-  <div class="w-full md:w-1/2 mb-4 pr-4">
+<main class="bg-background text-text flex flex-wrap justify-between p-8">
+  <div class="w-full md:w-1/2 mb-4">
     <h1 class="text-4.5xl font-title mb-4">Nos Missions</h1>
     <h3 class="mb-2">La mission de SquiReal est environnementale, le but est de sensibiliser et d’inculquer des valeurs écologiques afin de pouvoir faire face aux enjeux climatiques actuels pour un grand nombre d'utilisateurs.</h3>
-    <p class="mb-4">SquiReal s'engage passionnément dans une mission environnementale cruciale, axée sur la sensibilisation et l'inculcation de valeurs écologiques essentielles. Dans un monde confronté à des défis climatiques sans précédent, notre objectif est de mobiliser un vaste public pour faire face à ces enjeux cruciaux.</p>
+    <p class="mb-4 ">SquiReal s'engage passionnément dans une mission environnementale cruciale, axée sur la sensibilisation et l'inculcation de valeurs écologiques essentielles. Dans un monde confronté à des défis climatiques sans précédent, notre objectif est de mobiliser un vaste public pour faire face à ces enjeux cruciaux.</p>
     <p class="mb-4">Notre vision repose sur la conviction que chaque individu, quelle que soit sa situation ou son origine, a un rôle à jouer dans la préservation de notre planète. À travers nos initiatives et nos plateformes, nous aspirons à éduquer et à inspirer les gens à adopter des pratiques plus durables dans leur vie quotidienne.</p>
     <p class="mb-4">SquiReal offre une multitude de ressources et d'outils pour accompagner les utilisateurs dans leur parcours vers un mode de vie plus respectueux de l'environnement. Que ce soit par le biais de conseils pratiques, de ressources éducatives ou de communautés engagées, nous visons à fournir un soutien complet à tous ceux qui cherchent à faire une différence positive pour la planète.</p>
     <p class="mb-4">Nous croyons fermement en l'importance de l'action collective. En unissant nos efforts, nous pouvons avoir un impact significatif sur la lutte contre le changement climatique et la préservation de la biodiversité. Chaque petit geste compte, et ensemble, nous avons le pouvoir de créer un avenir plus vert et plus durable pour les générations à venir.</p>
     <p class="mb-4">À travers notre mission environnementale, SquiReal s'efforce de catalyser un changement de mentalité et d'inspirer une nouvelle génération de défenseurs de l'environnement. Nous sommes résolus à relever les défis qui se dressent devant nous et à œuvrer ensemble pour un avenir où l'harmonie entre l'homme et la nature prévaudra.</p>
   </div>
-  <img src="<?= base_url() ?>image/face.png" alt="mascotte 1" class="h-auto md:h- mb-4 ml-auto">
+  <div class="w-full md:w-1/2 mb-4 flex justify-end">
+    <img src="<?= base_url() ?>image/face.png" alt="mascotte 1" class="h-auto w-86">
+  </div>
 </main>
