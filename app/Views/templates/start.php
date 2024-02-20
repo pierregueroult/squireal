201cc05c19f4a7a4dc0d453fe995667be3f9d208
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-02-20 10:17:07 +0100

    Mise à jour des fichiers PWA

--- app/Views/templates/start.php
+++ app/Views/templates/start.php
@@ -5,11 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?= base_url() ?>/css/style.css">
+  <?php // Partie PWA   ?>
+  <link rel="manifest" href="<?= base_url() ?>manifest.webmanifest">
+  <meta name="theme-color" content="#FE9C4F">
   <title>
     SquiReal 🐿️ |
     <?= esc($title) ?>
   </title>
-  <link rel="manifest" href="<?= base_url() ?>manifest.webmanifest">
 </head>
 
 <body>
