afd01179a404de7f44f4753d04f76254a59892ef
lefebsa9 <sachalefebvre15@gmail.com>
2024-02-20 13:35:34 +0100

    h1 et h3 dans mission.php

--- app/Views/pages/mission.php
+++ app/Views/pages/mission.php
@@ -1,3 +1,4 @@
 <main>
-
+<h1>Our Missions</h1>
+<h3>SquiReal's mission is environmental, the goal is to raise awareness and instill ecological values ​​in order to be able to face current climate issues for large numbers of users.</h3>
 </main>
