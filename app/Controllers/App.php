5f454390ed9f0a51944efdaf942ae8f4c60c1239
Pierre Guéroult <contact@pierregueroult.dev>
2024-02-22 18:40:24 +0100

    Implementation du header de l'application

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -4,70 +4,71 @@ namespace App\Controllers;
 
 class App extends BaseController
 {
-    public function feed(): string|\CodeIgniter\HTTP\RedirectResponse
-    {
-        if (!$this->session->get("user")) {
-            $locale = $this->request->getLocale();
-            return redirect()->to(base_url() . $locale . "/app/auth");
-        }
-
-        $data = [
-            "title" => "Feed",
-        ];
-
-        return view("templates/start", $data) . view("templates/end", $data);
+  public function feed(): string|\CodeIgniter\HTTP\RedirectResponse
+  {
+    if (!$this->session->get("user")) {
+      $locale = $this->request->getLocale();
+      return redirect()->to(base_url() . $locale . "/app/auth");
     }
 
+    $data = [
+      "title" => "Feed",
+    ];
 
-    public function map(): string|\CodeIgniter\HTTP\RedirectResponse
-    {
-        if (!$this->session->get("user")) {
-            $locale = $this->request->getLocale();
-            return redirect()->to(base_url() . $locale . "/app/auth");
-        }
-        $data = [
-            "title" => "Map",
-        ];
+    return view("templates/start", $data) .
+      view("components/app/header", $data) .
+      view("templates/end", $data);
+  }
 
-        return view("templates/start", $data) . view("templates/end", $data);
+  public function map(): string|\CodeIgniter\HTTP\RedirectResponse
+  {
+    if (!$this->session->get("user")) {
+      $locale = $this->request->getLocale();
+      return redirect()->to(base_url() . $locale . "/app/auth");
     }
+    $data = [
+      "title" => "Map",
+    ];
 
-    public function camera(): string|\CodeIgniter\HTTP\RedirectResponse
-    {
-        if (!$this->session->get("user")) {
-            $locale = $this->request->getLocale();
-            return redirect()->to(base_url() . $locale . "/app/auth");
-        }
-        $data = [
-            "title" => "Camera",
-        ];
+    return view("templates/start", $data) . view("templates/end", $data);
+  }
 
-        return view("templates/start", $data) . view("templates/end", $data);
+  public function camera(): string|\CodeIgniter\HTTP\RedirectResponse
+  {
+    if (!$this->session->get("user")) {
+      $locale = $this->request->getLocale();
+      return redirect()->to(base_url() . $locale . "/app/auth");
     }
+    $data = [
+      "title" => "Camera",
+    ];
 
-    public function chat(): string|\CodeIgniter\HTTP\RedirectResponse
-    {
-        if (!$this->session->get("user")) {
-            $locale = $this->request->getLocale();
-            return redirect()->to(base_url() . $locale . "/app/auth");
-        }
-        $data = [
-            "title" => "Chat",
-        ];
+    return view("templates/start", $data) . view("templates/end", $data);
+  }
 
-        return view("templates/start", $data) . view("templates/end", $data);
+  public function chat(): string|\CodeIgniter\HTTP\RedirectResponse
+  {
+    if (!$this->session->get("user")) {
+      $locale = $this->request->getLocale();
+      return redirect()->to(base_url() . $locale . "/app/auth");
     }
+    $data = [
+      "title" => "Chat",
+    ];
 
-    public function profile(): string|\CodeIgniter\HTTP\RedirectResponse
-    {
-        if (!$this->session->get("user")) {
-            $locale = $this->request->getLocale();
-            return redirect()->to(base_url() . $locale . "/app/auth");
-        }
-        $data = [
-            "title" => "Profile",
-        ];
+    return view("templates/start", $data) . view("templates/end", $data);
+  }
 
-        return view("templates/start", $data) . view("templates/end", $data);
+  public function profile(): string|\CodeIgniter\HTTP\RedirectResponse
+  {
+    if (!$this->session->get("user")) {
+      $locale = $this->request->getLocale();
+      return redirect()->to(base_url() . $locale . "/app/auth");
     }
+    $data = [
+      "title" => "Profile",
+    ];
+
+    return view("templates/start", $data) . view("templates/end", $data);
+  }
 }
