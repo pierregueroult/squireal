4a5f64dcd668546559cd957bf5cddc3dfc28d734
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-02-22 15:49:26 +0100

    Changement de l'image page Authentification

--- app/Views/pages/auth.php
+++ app/Views/pages/auth.php
@@ -4,7 +4,7 @@ $locale = service('request')->getLocale();
 
 ?>
 <main class="no-padding overflow-hidden h-100vh">
-  <img src="<?= base_url() ?>image/squirel.jpg" class="h-full w-full object-cover fixed inset-0" />
+  <img src="<?= base_url() ?>image/Illustration.png" class="h-1/2 w-full object-cover fixed inset-0" />
   <section class="bg-background h-4/6 w-full fixed bottom-0 left-0 rounded-t-5xl p-8 flex flex-col gap-8">
     <?= view("components/app/logo") ?>
     <?= view("components/app/link", [
