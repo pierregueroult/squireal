68bf3796bb174dc63b850ac12cbbb75007db38bc
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-02-23 11:14:46 +0100

    Fix du Z du header de l'application

--- app/Views/components/app/header.php
+++ app/Views/components/app/header.php
@@ -1,15 +1,13 @@
-<header
-    class="fixed top-0 inset-x-0 flex items-center justify-between px-8 py-6 bg-transparent"
->
-    <h3
-        class="text-4xl font-title text-mainorange"
-    >
+<header class="fixed top-0 inset-x-0 flex items-center justify-between px-8 py-6 bg-transparent z-50">
+    <h3 class="text-4xl font-title text-mainorange">
         SquiReal
     </h3>
-    <a
-        href="<?= base_url() . "app/profile" ?>"
-
-    >
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user h-8 w-8"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-</a>
+    <a href="<?= base_url() . "app/profile" ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="lucide lucide-user h-8 w-8">
+            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+            <circle cx="12" cy="7" r="4" />
+        </svg>
+    </a>
 </header>
