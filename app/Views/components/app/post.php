897b5015106dc53ee77c51c86e523738e434f7e1
lefebsa9 <sachalefebvre15@gmail.com>
2024-03-11 10:26:24 +0100

    post.php

---
+++ app/Views/components/app/post.php
@@ -0,0 +1,28 @@
+<?php
+
+$userModel = new \App\Models\User();
+$user = $userModel->getFromId($post["userId"]);
+
+?>
+<article class="w-full rounded-lg p-2 flex flex-col space-y-2 border-maindarkgreen border-2">
+    <div class="flex items-center gap-2">
+        <img src="<?= base_url("image/man1.jpg") ?>" class="h-12 w-12 rounded-full object-cover" />
+        <div class="font-main space-y-1">
+            <p class="font-semibold text-md leading-none">
+                <?= $user["name"] ?>
+            </p>
+            <p class="text-sm leading-none">
+                @
+                <?= $user["username"] ?>
+            </p>
+        </div>
+    </div>
+    <p class="leading-5">
+        <?= $post["description"] ?>
+    </p>
+    <img src="<?= base_url("image/image_post1.jpg") ?>" class="max-h-32 object-cover rounded-lg" />
+    <div class="flex font-main text-sm gap-2">
+        <button class="bg-maindarkgreen text-white rounded-lg py-1 flex-1">Sauvegarder</button>
+        <button class="bg-maindarkgreen text-white rounded-lg py-1 flex-1">Partager</button>
+    </div>
+</article>
