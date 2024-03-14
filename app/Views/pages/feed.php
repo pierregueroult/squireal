5a00c2b0b5145631c73430e35f1a44e2906de65f
Pierre Guéroult <contact@pierregueroult.dev>
2024-03-14 20:06:39 +0100

    Fix du spacing de la page feed

--- app/Views/pages/feed.php
+++ app/Views/pages/feed.php
@@ -1,16 +1,16 @@
 <main class="no-padding">
     <p class="font-main px-4 pt-20 font-semibold">Posts recommandés :</p>
-    <section class="p-4">
+    <section class="px-4 py-2 space-y-4">
         <?php
         $postModel = new \App\Models\Post();
         $posts = $postModel->getLatest(10);
 
         foreach ($posts as $post) {
-            echo view("components/app/post", ["post" => $post]);
+          echo view("components/app/post", ["post" => $post]);
         }
         ?>
     </section>
-    <a href="<?= base_url() ?><?= service('request')->getLocale() ?>/app/post/create"
+    <a href="<?= base_url() . service("request")->getLocale() ?>/app/post/create"
         class="fixed bottom-28 right-8 py-4 px-6 bg-maindarkgreen rounded-xl text-foreground font-main text-sm">
         Créer un post
     </a>
