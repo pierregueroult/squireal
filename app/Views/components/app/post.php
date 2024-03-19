802c3d6ea94788c28bb296e4f4db165457251277
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-03-19 13:55:40 +0100

    Fix Color des posts

--- app/Views/components/app/post.php
+++ app/Views/components/app/post.php
@@ -3,7 +3,7 @@
 $userModel = new \App\Models\User();
 $user = $userModel->getFromId($post["userId"]);
 ?>
-<article class="w-full rounded-lg p-2 flex flex-col space-y-2 border-maindarkgreen border-2">
+<article class="w-full rounded-lg p-2 flex flex-col space-y-2 border-maindarkgreen border-2 bg-foreground">
     <div class="flex items-center gap-2">
         <img src="<?= base_url("image/man1.jpg") ?>" class="h-12 w-12 rounded-full object-cover" />
         <div class="font-main space-y-1">
