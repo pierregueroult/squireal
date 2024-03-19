28048fdcac3edf43db7d810b53f0f27b379c3317
lefebsa9 <sachalefebvre15@gmail.com>
2024-03-19 15:16:49 +0100

    Page d'accueil

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,4 +1,18 @@
-<main>
-  Home page
-  <?= lang("Home.main-title") ?>
+<main class="flex justify-between">
+  <div>
+  <h1 class="font-main font-bold text-6xl p-8">Bien plus qu'une <br> simple application<br> écologique.</h1>
+  <br><br>
+  <p class="font-main font-semibold text-3xl p-8">Une application <span class="font-title text-mainorange font-normal">GRATUITE</span><br> qui évolue grâce à ses utilisateurs.</p>
+  <p class="font-main font-semibold text-3xl p-8">Une applictation <span class="font-title text-mainorange font-normal">ENGAGÉE </span><br> contre le réchauffement climatique.</p>
+  <p class="font-main font-semibold text-3xl p-8">Une application <span class="font-title text-mainorange font-normal">COMMUNAUTAIRE</span><br> qui lutte avec vous pour un monde plus durable.</p>
+</div>
+<div>
+  <div id="boite" class="bg-black h-32 w-32 m-28 rounded-2xl"></div>
+</div>
+<style>
+#boite{
+  width: 30vw;
+  aspect-ratio: 4/5;
+}
+</style>
 </main>
