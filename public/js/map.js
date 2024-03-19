f12bf0d0fa7c2bc365999607fc3c328a2bd172c0
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-03-19 09:19:53 +0100

    Centrage de la carte sur la localisation actuelle

--- public/js/map.js
+++ public/js/map.js
@@ -5,8 +5,12 @@ mapboxgl.accessToken = mapbox_token;
 const map = new mapboxgl.Map({
   container: "map-container",
   style: "mapbox://styles/mapbox/streets-v11",
-  center: [1.09932, 49.44313],
-  zoom: 12,
+  center: [2.333333, 48.866667], // starting at Paris
+  zoom: 11,
+});
+
+navigator.geolocation.getCurrentPosition((position) => {
+  map.setCenter([position.coords.longitude, position.coords.latitude]);
 });
 
 const setMarkers = async () => {
@@ -29,7 +33,7 @@ const setMarkers = async () => {
         new mapboxgl.Popup({ offset: 25 }).setHTML(`<div class="w-48 flex flex-col items-center">
         <h3 class="text-lg font-main font-bold">${event.name}</h3>
         <p class="font-main font-semibold">${event.description}</p>
-        <a class="bg-maindarkgreen text-white font-main rounded-lg py-2 px-4 mt-4" href="${base_url}/event/${event.id}">Rejoindre l'événement</a>
+        <a class="bg-maindarkgreen text-white font-main rounded-lg py-2 px-4 mt-4" href="${base_url}${locale}/app/event/join/${event.event_id}">Rejoindre l'événement</a>
       </div>`),
       );
   });
