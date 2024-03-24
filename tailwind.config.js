4524f3aadc022468a6a8344f72b7d5318a8f2427
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-03-24 17:18:48 +0100

    Mise à jour de la config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     "./app/Views/**/*.{html,js,php}",
   ],
   theme: {
+    screens: {
+      msm: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         background: "#FEFEDE",
