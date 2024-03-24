9ff3655b9895b7d876808e7da071c72b9ce50d66
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-03-24 08:59:42 +0100

    Fix de l'image de profil par défaut pour les events

--- app/Views/components/app/event.php
+++ app/Views/components/app/event.php
@@ -7,7 +7,7 @@ $user = $userModel->getFromId($event["owner_id"]);
 
 <article class="w-full rounded-lg p-2 flex flex-col space-y-2 border-maindarkgreen border-2">
   <div class="flex items-center gap-2">
-    <img src="<?= base_url("image/man1.jpg") ?>" class="h-12 w-12 rounded-full object-cover" />
+    <img src="<?= base_url("image/blank.webp") ?>" class="h-12 w-12 rounded-full object-cover" />
     <div class="font-main space-y-1">
       <p class="font-semibold text-md leading-none">
         <?= $user["name"] ?>
