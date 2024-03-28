61969c99a321e1c42d997451de7fb726142a8cf0
Pierre Guéroult <81022221+pierregueroult@users.noreply.github.com>
2024-03-28 08:04:32 +0100

    Mise a jour favicon

--- app/Views/templates/start.php
+++ app/Views/templates/start.php
@@ -5,9 +5,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?= base_url() ?>/css/style.css">
-  <?php // Partie PWA    ?>
+  <?php // Partie PWA     ?>
   <link rel="manifest" href="<?= base_url() ?>manifest.webmanifest" crossorigin="use-credentials">
   <meta name="theme-color" content="#FE9C4F">
+  <link rel="icon" href="<?= base_url() ?>favicon.ico" type="image/x-icon">
   <title>
     SquiReal 🐿️ |
     <?= esc($title) ?>
